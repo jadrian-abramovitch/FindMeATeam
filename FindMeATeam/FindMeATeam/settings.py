88967b01ca8e408a888cc541aa4bdc92ea6bf476
unknown <you@example.com>
2020-10-24 18:59:18 -0400

    changing db connection settings

--- FindMeATeam/FindMeATeam/settings.py
+++ FindMeATeam/FindMeATeam/settings.py
@@ -81,12 +81,16 @@ WSGI_APPLICATION = 'FindMeATeam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # import dj_database_url
 # db_from_env = dj_database_url.config()
