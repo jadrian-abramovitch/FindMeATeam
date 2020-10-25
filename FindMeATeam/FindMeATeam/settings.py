65fc4761526f9d24bfe2c17b93181871897ff391
unknown <you@example.com>
2020-10-24 20:24:05 -0400

    updating settings

--- FindMeATeam/FindMeATeam/settings.py
+++ FindMeATeam/FindMeATeam/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'FindMeATeam.wsgi.application'
 
 
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 # import dj_database_url
 # db_from_env = dj_database_url.config()
