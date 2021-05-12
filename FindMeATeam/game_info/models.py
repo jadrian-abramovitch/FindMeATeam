78a71951b497bd87caba327fa8910ddcf6ef902f
Jadrian Maxwell Abramovitch <jmabramovitch@uwaterloo.ca>
2021-05-12 15:37:42 -0400

    removing icons

--- FindMeATeam/game_info/models.py
+++ FindMeATeam/game_info/models.py
@@ -63,7 +63,7 @@ class GameInfo(models.Model):
 
 
 	game = models.CharField(max_length=20, choices=games, default=LEAGUE_OF_LEGENDS)
-	game_icon = models.CharField(max_length=40, default=game_icons[LEAGUE_OF_LEGENDS])
+	# game_icon = models.CharField(max_length=40, default=game_icons[LEAGUE_OF_LEGENDS])
 	positions_played = models.CharField(max_length=7, choices=positions, default='Top')
 	rank = models.CharField(max_length=20, choices=ranks, default='Challenger')
 	server = models.CharField(max_length=5, choices=servers, default='NA')
@@ -79,6 +79,6 @@ class GameInfo(models.Model):
 	def get_absolute_url(self):
 		return reverse('home')
 
-	def save(self):
-		game_icon = game_icons[game]
-		super().save()
+	# def save(self):
+	# 	game_icon = game_icons[game]
+	# 	super().save()
